dc1ad63634530b5bc95554ddb86513eda049b27c
Stephanie Fernandes <steph3001@gmail.com>
2020-01-20 15:46:44 -0300

    Added Search functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import Search from './components/Search/index';
+// import CategoryList from './components/CategoryList/index';
+import EmojiList from './components/EmojiList/index';
+import Footer from './components/Footer/index';
+
+import './global.scss';
+import './App.scss';
 
 function App() {
+
+  const [allEmojis, setAllEmojis] = useState([]);
+  const [emojis, setEmojis] = useState([]);
+  // const [categories, setCategories] = useState([]);
+  const [search, setSearch] = useState('');
+
+  
+  function updateSearch(text) {
+    setSearch(text);
+    console.log(search, text)
+    const emojiList =  allEmojis.filter((text) => (
+      text.unicodeName.toLowerCase().indexOf(search.toLowerCase()) !== -1
+    ));
+    setEmojis(emojiList);
+  }
+  
+  useEffect(() => {
+    async function loadEmojis() {
+      const response = await api.get('/emojis');
+      setAllEmojis(response.data);
+    }
+    loadEmojis();
+  }, []);
+
+  // useEffect(() => {
+  //   async function loadEmojisFromCategory() {
+  //     const response = await api.get('/emojis');
+  //     console.log(response.data);
+  //     // setEmojis(response.data);
+  //   }
+  //   loadEmojisFromCategory();
+  // }, []);
+
+  // useEffect(() => {
+  //   async function loadCategories() {
+  //     const response = await api.get('/categories');
+  //     setCategories(response.data);
+  //   }
+  //   loadCategories();
+  // },[]);  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <img className='logo' src='./emojo.svg' alt='emojo logo' />
+        <Search updateSearch={updateSearch} />
       </header>
+      <EmojiList emojiList={emojis} />
+      {/* <CategoryList categories={categories} /> */}
+      <Footer />
     </div>
   );
 }
