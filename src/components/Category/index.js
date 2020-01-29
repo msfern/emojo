2b60c1d580f56388e8051f7da9956eb424c43c4f
Stephanie Fernandes <steph3001@gmail.com>
2020-01-29 20:16:54 -0300

    Formated Category slug

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -3,11 +3,15 @@ import React from 'react';
 import './style.scss';
 
 function Category({ details }) {
+  const formatCategoryTitle = (title) => {
+    const formattedTitle = title.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' & ');
+    return formattedTitle;
+  }
 
   return (
-    <div>
-      <p>{details.slug}</p>
-      <p>{details.emoji}</p>
+    <div className='category'>
+      <span className='category-emoji'>{details.emoji}</span>
+      <p className='category-name'>{formatCategoryTitle(details.slug)}</p>
     </div>
   );
 }
