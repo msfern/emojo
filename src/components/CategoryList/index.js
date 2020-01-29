f58a3c293d94117b3849d7539f141653179e33ee
Stephanie Fernandes <steph3001@gmail.com>
2020-01-29 20:16:06 -0300

    Added title to Category List

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -7,11 +7,14 @@ import './style.scss';
 function CategoryList({ categories }) {
 
   return (
-    <div className='category-list'>
-      { Object.keys(categories).map((category) => (
-        <Category key={category} details={categories[category]} />
-      ))}
-    </div>
+    <>
+      <h2 className='category-title'>Categories</h2>
+      <div className='category-list'>
+        { Object.keys(categories).map((category) => (
+          <Category key={category} details={categories[category]} />
+        ))}
+      </div>
+    </>
   );
 }
 
