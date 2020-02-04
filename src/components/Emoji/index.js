bba37d07e7a82f1bf296468abfe3673c0e0869a9
Stephanie Fernandes <steph3001@gmail.com>
2020-02-04 00:12:02 -0300

    Installed Clipboard lib

--- src/components/Emoji/index.js
+++ src/components/Emoji/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Clipboard from 'clipboard';
 
 import './style.scss';
 
 function Emoji({ slug, emoji }) {
+
+  useEffect(() => {
+    new Clipboard('.emoji-card');
+  },[]);
+  
   return (
-    <div className='emoji-card'>
+    <div className='emoji-card' data-clipboard-text={emoji}>
       <span className='emoji-char'>{emoji}</span>
       <p className='emoji-slug'>{slug}</p>
     </div>
